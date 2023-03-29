91d8b7eaba5234302d85f51f4eef044bcce8deca
eddynazario <eddynazario@gmail.com>
2023-03-29 14:12:28 -0500

    added documentation

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -13,11 +13,11 @@ class ProductController extends Controller
         $products = Product::all();
         // Check for products
         if ($products->count() > 0){
-            return response()->json($products, 200);
+            return response()->json($products, 200); // Return all products and OK HTTP response
         } else {
             // No products found
             return response()->json([
-                'status' => 404,
+                'status' => 404, // Send not found HTTP response
                 'message' => 'No product records found.'
             ], 404);
         }
@@ -33,7 +33,7 @@ class ProductController extends Controller
 
         if ($validator->fails()) {
             return response()->json([
-                'status' => 422,
+                'status' => 422, // Send unprocessable content HTTP response
                 'errors' => $validator->messages()
             ], 422);
         } else {
@@ -46,12 +46,12 @@ class ProductController extends Controller
 
             if ($product) {
                 return response()->json([
-                    'status' => 200,
+                    'status' => 200, // Send OK HTTP response
                     'product' => 'Product added successfully.'
                 ], 200);
             }else {
                 return response()->json([
-                    'status' => 500,
+                    'status' => 500, // Send unexpected server error HTTP response
                     'message' => 'Servers were nuked out of orbit.'
                 ], 500);
             }
